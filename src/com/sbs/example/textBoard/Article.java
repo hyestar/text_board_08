9fa2fb9fe93044089af29918c7af1e809dbfd4e6
hyestar <0109jhs@naver.com>
2021-08-19 12:22:02 +0900

    게시판 추가, 조회 기능 구현

--- src/com/sbs/example/textBoard/Article.java
+++ src/com/sbs/example/textBoard/Article.java
@@ -1,7 +1,21 @@
 package com.sbs.example.textBoard;
 
-public class Article {
+public class Article extends Object {
 	public int id;
 	public String title;
-	public String content;
+	public String body;
+	
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", title=" + title + ", body=" + body + "]";
+		
+	}
+	
+	
 }
--- src/com/sbs/example/textBoard/Main.java
+++ src/com/sbs/example/textBoard/Main.java
@@ -5,10 +5,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		List<Article> articles = new ArrayList<>();
+		int lastArticleId = 0;
 		
 		while(true) {
 			System.out.printf("명령어를 입력해주세요) ");
@@ -19,7 +20,28 @@ public class Main {
 				break;
 			}
 			
+			else if(cmd.equals("add")) {
+				int id = lastArticleId + 1;
+				System.out.print("제목 : ");
+				String title = sc.nextLine();
+				System.out.print("내용 : ");
+				String body = sc.nextLine();
+				
+				Article article = new Article(id, title, body);
+				articles.add(article);
+				lastArticleId++;
+				
+				System.out.println(article);
+				
+			}
 			else if(cmd.equals("list")) {
+				if(articles.size()==0) {
+					System.out.println("게시물이 존재하지 않습니다.");
+					continue;
+				}
+				for(Article article : articles) {
+					System.out.printf("아이디 : %d   제목 : %s\n", article.id, article.title);
+				}
 				
 			}
 		}
