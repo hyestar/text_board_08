089928dd14848f8c948d3fb7dbcb4544dd0880f9
hyestar <0109jhs@naver.com>
2021-08-20 12:16:32 +0900

    JDBC insert test

--- db.sql
+++ db.sql
@@ -1,2 +1,19 @@
 DROP DATABASE IF EXISTS text_board;
 CREATE DATABASE text_board;
+USE text_board;
+
+CREATE TABLE article(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(100) NOT NULL,
+    `body` TEXT NOT NULL
+);
+
+SELECT * FROM article;
+
+INSERT INTO article 
+SET regDate = NOW(),
+updateDate = NOW(), 
+title = CONCAT('제목', ROUND(RAND()*10)), 
+`body` = CONCAT('내용', ROUND(RAND()*10));
---
+++ src/com/sbs/example/textBoard/test/JDBCInsertTest.java
@@ -0,0 +1,59 @@
+// ctrl shift enter 정렬
+package com.sbs.example.textBoard.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class JDBCInsertTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Connection conn = null;
+			PreparedStatement pstat = null;
+
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+				conn = DriverManager.getConnection(url, "root", "");
+				System.out.println("연결 성공!");
+				
+				
+				String sql = "INSERT INTO article"; 
+				sql +=	" SET regDate = NOW()";
+				sql +=	", updateDate = NOW()" ;
+				sql +=	", title = CONCAT('제목', ROUND(RAND()*10))";
+				sql +=	", `body` = CONCAT('내용', ROUND(RAND()*10));";
+				
+				pstat = conn.prepareStatement(sql);
+				int affectedRows = pstat.executeUpdate();
+				
+				System.out.println("affectedRows : " + affectedRows);
+
+			} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+			} catch (SQLException e) {
+					System.out.println("에러: " + e);
+			} finally {
+				try {
+					if (conn != null && !conn.isClosed()) {
+						conn.close();
+					}
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				try {
+					if (pstat != null && !pstat.isClosed()) {
+						pstat.close();
+					}
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		
+	}
+
+}
