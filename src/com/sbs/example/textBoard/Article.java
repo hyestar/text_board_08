0348ac7ce4a5458ff8c8837e9158f9f84323fb99
hyestar <0109jhs@naver.com>
2021-08-24 11:12:09 +0900

    JDBC UPDATE TEST

--- src/com/sbs/example/textBoard/Article.java
+++ src/com/sbs/example/textBoard/Article.java
@@ -15,6 +15,13 @@ public class Article extends Object {
 		this.body = body;
 	}
 
+	public Article(int id, String title, String body) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+
 	@Override
 	public String toString() {
 		return "Article [id=" + id + ", regDate=" + regDate + ", updateDate=" + updateDate + ", title=" + title
