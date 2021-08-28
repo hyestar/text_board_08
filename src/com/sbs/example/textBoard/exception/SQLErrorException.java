cbcd23a9d82561803da0dd4ebc5faa317d852814
hyestar <0109jhs@naver.com>
2021-08-28 23:46:15 +0900

    SQLErrorException

---
+++ src/com/sbs/example/textBoard/exception/SQLErrorException.java
@@ -0,0 +1,14 @@
+package com.sbs.example.textBoard.exception;
+
+public class SQLErrorException extends RuntimeException {
+	private Exception origin;
+
+	public SQLErrorException(String message, Exception origin) {
+		super(message);
+		this.origin = origin;
+	}
+
+	public Exception getOrigin() {
+		return origin;
+	}
+}
